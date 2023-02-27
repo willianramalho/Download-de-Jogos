69891c67ad7ee2058e603d01064a3735a622c062
Willian VInicius Ramalho <49045509+willianramalho@users.noreply.github.com>
2023-02-27 15:46:26 -0300

    Adicionado funcionalidades ao Js
    É feito a verificação de quando o botão é pressionado ele exibe um alert e apos o alert ele recarrega a pagina

---
+++ loja/animacao.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById('botaoPesquisa');
+
+botao.addEventListener("click", function (){alert("botao pressionado")} );
+
+if(botao.addEventListener("click", function (){window.location.reload()})){
+}
